aa0cb7f712ada15855a63bf2ab063773afc41c90
predd5679 <pranavreddy8888@gmail.com>
2023-04-18 23:25:38 -0400

    Route for p/get_review_made and its thunderclient test

--- flask-app/src/products/products.py
+++ flask-app/src/products/products.py
@@ -59,3 +59,28 @@ def get_most_pop_products():
 
     return jsonify(json_data)
 
+
+@products.route('/Review_made', methods=['GET'])
+def get_review_made():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT CustomerID, ProductID, n_stars, comment FROM Review_made')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
